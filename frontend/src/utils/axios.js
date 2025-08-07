4f833513ffb27202d32297599d4dcf4c9e686dbb
mahi13singh2004 <mahi13singh2004@gmail.com>
2025-08-07 13:51:59 +0530

    chnaged url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://batcheet-ai-backend.onrender.com",
     withCredentials:true,
 })
 
